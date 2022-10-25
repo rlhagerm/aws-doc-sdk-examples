ad07c97c4437f1b886b8939ca8c3bdfa342579eb
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2022-10-25 11:37:13 -0500

    Adding tests and settings.

--- dotnetv3/cross-service/AuroraItemTracker/ItemTrackerTests/WorkItemServiceTests.cs
+++ dotnetv3/cross-service/AuroraItemTracker/ItemTrackerTests/WorkItemServiceTests.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier:  Apache-2.0
 
 using Amazon.RDSDataService;
+using Amazon.RDSDataService.Model;
 using AuroraItemTracker;
 using Microsoft.Extensions.Configuration;
 namespace ItemTrackerTests;
@@ -31,10 +32,25 @@ public class WorkItemServiceTests
 
     [Fact]
     [Order(1)]
+    [Trait("Category", "Integration")]
     public async Task VerifyCanExecuteRDSStatement_ShouldReturnResult()
     {
         var tableName = _configuration["WorkItemTable"];
         var result = await _workItemService.ExecuteRDSStatement($"SELECT * FROM {tableName};");
         Assert.NotNull(result.FormattedRecords);
+    }
+
+    [Fact]
+    [Order(2)]
+    [Trait("Category", "Unit")]
+    public void VerifyGetItemsFromResponse_ShouldReturnCollection()
+    {
+        var responseString = _configuration["WorkItemCollectionResponseString"];
+        ExecuteStatementResponse response = new ExecuteStatementResponse()
+        {
+            FormattedRecords = responseString
+        };
+        var workItems = _workItemService.GetItemsFromResponse(response);
+        Assert.Equal(2, workItems.Length);
     }
 }
