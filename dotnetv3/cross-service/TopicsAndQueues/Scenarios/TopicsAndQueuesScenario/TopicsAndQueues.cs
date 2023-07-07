de9829bb945574671cfce72d2f3ed31a9cb2d52d
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2023-07-07 09:48:30 -0500

    Work in progress for workflow.

---
+++ dotnetv3/cross-service/TopicsAndQueues/Scenarios/TopicsAndQueuesScenario/TopicsAndQueues.cs
@@ -0,0 +1,64 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+using SNSActions;
+using SQSActions;
+
+namespace TopicsAndQueuesScenario;
+
+// snippet-start:[TopicsAndQueues.dotnetv3.Scenario]
+public class TopicsAndQueues
+{
+    private static ILogger logger = null!;
+    private static SNSWrapper _snsWrapper = null!;
+    private static SQSWrapper _sqsWrapper = null!;
+    private static IConfiguration _configuration = null!;
+
+    static async Task Main(string[] args)
+    {
+        // Set up dependency injection for Amazon EventBridge.
+        using var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+                logging.AddFilter("System", LogLevel.Debug)
+                    .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Information)
+                    .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Trace))
+            .ConfigureServices((_, services) =>
+                services.AddAWSService<IAmazonSQS>()
+                    .AddAWSService<IAmazonSimpleNotificationService>()
+                    .AddTransient<SNSWrapper>()
+                    .AddTransient<SQSWrapper>()
+            )
+            .Build();
+
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("settings.json") // Load settings from .json file.
+            .AddJsonFile("settings.local.json",
+                true) // Optionally, load local settings.
+            .Build();
+
+        logger = LoggerFactory.Create(builder => { builder.AddConsole(); })
+            .CreateLogger<TopicsAndQueues>();
+
+        ServicesSetup(host);
+    }
+
+    /// <summary>
+    /// Populate the services for use within the console application.
+    /// </summary>
+    /// <param name="host">The services host.</param>
+    private static void ServicesSetup(IHost host)
+    {
+        _snsWrapper = host.Services.GetRequiredService<SNSWrapper>();
+        _sqsWrapper = host.Services.GetRequiredService<SQSWrapper>();
+    }
+}
+// snippet-end:[TopicsAndQueues.dotnetv3.Scenario]
