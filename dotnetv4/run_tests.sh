f991c5d5949d64d4688ba1e637d22094eeee318e
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2025-05-15 15:06:14 -0500

    test script.

--- dotnetv4/run_tests.sh
+++ dotnetv4/run_tests.sh
@@ -3,8 +3,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
 echo "Running tests..."
-dotnet vstest --logger:trx/LogFileName=dotnetv4_results.trx '@/dotnetv4/testsettings.txt'
-dotnet new tool-manifest
-dotnet tool install DotnetWeathertopJsonReporter --local
-dotnet tool run DotnetWeathertopJsonReporter -t TestResults/dotnet4_results.trx
-echo "Test run complete."
+
